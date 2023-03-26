45537eddef6e9e99c3298543e87cfc1af043de98
Mahdi Khoshdel <47051507+mahdikhoshdel@users.noreply.github.com>
2023-03-26 14:35:22 +0330

    Add files
    Added main file and snake module include moving snake to any direction using arrow keys

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+# listen to input keys
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+game_on = True
+while game_on:
+    screen.update()
+    # snake delay
+    time.sleep(0.1)
+    snake.move()
+
+
+
+screen.exitonclick()
