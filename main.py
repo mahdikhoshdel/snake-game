6f26a6b1f6725b37e45347ac8e7e83917b4ddc8b
Mahdi Khoshdel <47051507+mahdikhoshdel@users.noreply.github.com>
2023-04-08 13:46:20 +0330

    adjust and update main

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from turtle import Screen
 from snake import Snake
 import time
 from food import Food
+from scoreboard import ScoreBoard
 
-# screen window config
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -12,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 # listen to input keys
 screen.listen()
@@ -21,6 +22,7 @@ screen.onkey(snake.right, "Right")
 screen.onkey(snake.left, "Left")
 
 game_on = True
+score_now = 0
 while game_on:
 
     screen.update()
@@ -31,6 +33,21 @@ while game_on:
     # Detect food collision
     if snake.head.distance(food) < 15:
         food.relocate()
+        snake.extend()
+        scoreboard.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or \
+            snake.head.xcor() < -280 or \
+            snake.head.ycor() > 280 or \
+            snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # Detect tail collision
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
