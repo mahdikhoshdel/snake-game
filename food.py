8caf0743e31255f26bd2b07edfc90388a10e29c0
Mahdi Khoshdel <47051507+mahdikhoshdel@users.noreply.github.com>
2023-04-08 13:46:48 +0330

    change food color

--- food.py
+++ food.py
@@ -9,7 +9,7 @@ class Food(Turtle):
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("white")
+        self.color("Blue")
         self.speed("fastest")
         self.relocate()
 
